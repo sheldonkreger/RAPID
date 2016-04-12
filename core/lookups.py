884d27852731712a2037e1b72c55b27ae6c3271f
Curtis Homan <choman@eitccorp.com>
2016-04-12 18:08:33 -0400

    Added new lookup function to get Google search results for a domain.  Also fixed a few very minor PEP findings regarding whitespace.

--- core/lookups.py
+++ core/lookups.py
@@ -4,12 +4,14 @@ import tldextract
 import pythonwhois
 import dns.resolver
 import geoip2.database
+import google
 from ipwhois import IPWhois
 from collections import OrderedDict
 from ipwhois.ipwhois import IPDefinedError
 from censys.ipv4 import CensysIPv4
 from censys.base import CensysException
 from django.conf import settings
+from urllib.parse import urlparse
 
 
 logger = logging.getLogger(__name__)
@@ -112,6 +114,7 @@ def lookup_ip_whois(ip):
 
     return None
 
+
 def lookup_ip_censys_https(ip):
     api_id = settings.CENSYS_API_ID
     api_secret = settings.CENSYS_API_SECRET
@@ -120,6 +123,52 @@ def lookup_ip_censys_https(ip):
         ip_data = CensysIPv4(api_id=api_id, api_secret=api_secret).view(ip)
         return ip_data['443']['https']['tls']['certificate']['parsed']
     except KeyError:
-        return {'status':404,'message':"No HTTPS certificate data was found for IP " + ip}
+        return {'status': 404, 'message': "No HTTPS certificate data was found for IP " + ip}
     except CensysException as ce:
-        return {'status':ce.status_code,'message':ce.message}
+        return {'status': ce.status_code, 'message': ce.message}
+
+
+def search_for_domain(domain, limit=10):
+    """Find the top 'limit' Google search results for 'domain' (excluding those from 'domain' itself)."""
+    logger.debug("Searching Google for domain '%s' (limit: %d)", domain, limit)
+    parameter = "\"" + domain + "\""
+    result = list()
+
+    # We actually need to filter our results to ensure that we are NOT including results from the actual domain being
+    # queried.  As a result, it might be necessary to query Google multiple times.  We will attempt to reduce this by
+    # asking Google for three times the number of requested results.  (On the one hand, we don't want to ask Google too
+    # frequently lest we be blocked.  On the other hand, we don't want excess results lest we waste time filtering.)
+    num = limit * 3
+    start = 0
+    stop = limit
+    try:
+        while True:
+            logger.debug("Checking Google (start: %d, stop: %d, num: %d)", start, stop, num)
+            urls = list(google.search(parameter, num=num, start=start, stop=stop))
+            logger.debug("Google results: %s", urls)
+            if not urls:
+                logger.warn("Only %d Google results were found for domain '%s' (requested %d)",
+                            len(result),
+                            domain,
+                            limit)
+                break
+            result += [url for url in urls if not urlparse(url).netloc.endswith(domain)]
+            logger.debug("Updated results: %s", result)
+
+            # It's possible that we could end up with more results than requested, in which case we need to take only
+            # the top 'limit' results.
+            if len(result) >= limit:
+                logger.debug("Maximum requested results met or exceeded (requested: %d, found %d)", limit, len(result))
+                result = result[:limit]
+                break
+
+            start = stop
+            stop += limit
+
+    except Exception:
+        # Something went wrong, most likely while querying Google.  There's nothing we can really do about it, so we
+        # will log the error and return our list as-is (which is probably empty)
+        logger.exception("Unexpected error performing google search")
+
+    logger.debug("Found top %d/%d search results for domain '%s': %s", len(result), limit, domain, result)
+    return result
